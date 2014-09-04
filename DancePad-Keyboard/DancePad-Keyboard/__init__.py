53eea582c0f9982b4051c63a1abc2a0a735c5204
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-09-04 15:22:45 -0400

    Got keyboard output working!

--- DancePad-Keyboard/DancePad-Keyboard/__init__.py
+++ DancePad-Keyboard/DancePad-Keyboard/__init__.py
@@ -1,9 +1,19 @@
-import serial, time
+import serial, time, uinput
 
 ser = serial.Serial('/dev/ttyUSB1', 9600)
 
+keyList = [uinput.KEY_1, uinput.KEY_0]
+
+device = uinput.Device(keyList)
+
+print 'Warning Keyboard will start outputting in 5 seconds!'
+
+time.sleep(5)
+
 while True:
     ser.write('255\n')
+    device.emit_click(uinput.KEY_1)
     time.sleep(.5)
     ser.write('0\n')
+    device.emit_click(uinput.KEY_0)
     time.sleep(.5)
