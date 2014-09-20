5f37d235d747c364a066d8f6042bd05f6405d0b4
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-09-20 18:45:21 -0400

    :soon::smile:

--- DancePad-Keyboard/DancePad-Keyboard/__init__.py
+++ DancePad-Keyboard/DancePad-Keyboard/__init__.py
@@ -1,19 +1,20 @@
 import serial, time, uinput
 
-ser = serial.Serial('/dev/ttyUSB1', 9600)
+ser = serial.Serial('/dev/ttyUSB0', 9600)
 
-keyList = [uinput.KEY_1, uinput.KEY_0]
+#keyList = [uinput.KEY_1, uinput.KEY_0]
 
-device = uinput.Device(keyList)
+#device = uinput.Device(keyList)
 
-print 'Warning Keyboard will start outputting in 5 seconds!'
+#print "Warning Keyboard will start outputting in 5 seconds!"
 
-time.sleep(5)
-
+#time.sleep(5)
+ser.write("a")
 while True:
-    ser.write('255\n')
-    device.emit_click(uinput.KEY_1)
-    time.sleep(.5)
-    ser.write('0\n')
-    device.emit_click(uinput.KEY_0)
-    time.sleep(.5)
+    a =  ser.read() 
+    #device.emit_click(uinput.KEY_1)
+    #time.sleep(.5)
+    ser.write(a)
+    print a
+    #device.emit_click(uinput.KEY_0)
+    #time.sleep(.5)
