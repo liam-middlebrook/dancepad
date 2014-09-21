260b49d29a19fca5d9d6ec1555a96eeefc52091e
Liam Middlebrook <liammiddlebrook@gmail.com>
2014-09-20 20:36:09 -0400

    :video_game::musical_keyboard::computer:

--- DancePad-Keyboard/DancePad-Keyboard/__init__.py
+++ DancePad-Keyboard/DancePad-Keyboard/__init__.py
@@ -2,9 +2,9 @@ import serial, time, uinput
 
 ser = serial.Serial('/dev/ttyUSB0', 9600)
 
-#keyList = [uinput.KEY_1, uinput.KEY_0]
+keyList = [uinput.KEY_LEFT, uinput.KEY_RIGHT, uinput.KEY_UP, uinput.KEY_DOWN]
 
-#device = uinput.Device(keyList)
+device = uinput.Device(keyList)
 
 #print "Warning Keyboard will start outputting in 5 seconds!"
 
@@ -16,5 +16,13 @@ while True:
     #time.sleep(.5)
     ser.write(a)
     print a
-    #device.emit_click(uinput.KEY_0)
+    a = ord(a)
+    if a & 1 is 1:
+        device.emit_click(uinput.KEY_LEFT)
+    if a & 2 is 2:
+        device.emit_click(uinput.KEY_DOWN)
+    if a & 4 is 4:
+        device.emit_click(uinput.KEY_UP)
+    if a & 8 is 8:
+        device.emit_click(uinput.KEY_RIGHT)
     #time.sleep(.5)
--- arduino/ddr_pad/ddr_pad.ino
+++ arduino/ddr_pad/ddr_pad.ino
@@ -1,13 +1,19 @@
 // pins for the LEDs:
 const int redPin = 13;
-const int buttonPin = 2;
+const int buttonOnePin = 2;
+const int buttonTwoPin = 3;
+const int buttonThreePin = 4;
+const int buttonFourPin = 5;
 
 void setup() {
   // initialize serial:
   Serial.begin(9600);
   // make the pins outputs:
   pinMode(redPin, OUTPUT); 
-  pinMode(buttonPin, INPUT); 
+  pinMode(buttonOnePin, INPUT); 
+  pinMode(buttonTwoPin, INPUT); 
+  pinMode(buttonThreePin, INPUT); 
+  pinMode(buttonFourPin, INPUT); 
 }
 
 void loop(){}
@@ -15,7 +21,11 @@ void serialEvent()
 {
     char charX= Serial.read();
     digitalWrite(redPin, charX == '1' ? HIGH : LOW);
-    
-    Serial.write(digitalRead(buttonPin) == HIGH ? 'A' : 'B');
+    int buttonID = 0;
+    buttonID += (digitalRead(buttonOnePin) == HIGH) ? 1 : 0;
+    buttonID += (digitalRead(buttonTwoPin) == HIGH) ? 2 : 0;
+    buttonID += (digitalRead(buttonThreePin) == HIGH) ? 4 : 0;
+    buttonID += (digitalRead(buttonFourPin) == HIGH) ? 8 : 0;
+    Serial.write( buttonID + '0');
 }
 
